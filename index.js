c2a7cda04488420e6d8f70bb728aa7d31b031e0b
Narendra <naren3001singh@gmail.com>
2021-02-18 18:49:31 +0530

    Added Functionality and Local Storage

---
+++ index.js
@@ -0,0 +1,59 @@
+//value of new task
+const task = document.getElementById('input-task');
+
+//event listener for add btn
+document.querySelector('.add-btn').addEventListener('click', addTask);
+//event listener for deleting
+document.querySelector('#container').addEventListener('dblclick', deleteTask);
+
+function addTask(e) {
+	if (task.value === '') {
+		alert('Field is Empty !!!');
+	} else {
+		//creating new li
+		const li = document.createElement('li');
+		//setting a class
+		li.setAttribute('class', 'task-item');
+		//putting value of task-input into it
+		li.appendChild(document.createTextNode(task.value));
+		//appending to ul parent
+		document.getElementById('task').appendChild(li);
+		//localstorage
+		let tasks;
+
+		if (localStorage.getItem('tasks') === null) {
+			tasks = [];
+		} else {
+			tasks = JSON.parse(localStorage.getItem('tasks'));
+		}
+
+		tasks.push(task.value);
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+
+		//clearing task input
+		task.value = '';
+	}
+
+	//preventing default behaviour of submit
+	e.preventDefault();
+}
+
+function deleteTask(e) {
+	if (e.target.classList.contains('task-item')) {
+		//localStorage
+		let tasks;
+		let newTasks = [];
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+
+		tasks.forEach((task) => {
+			if (task !== e.target.textContent) newTasks.push(task);
+		});
+		tasks.pop(e.target.textContent);
+		localStorage.setItem('tasks', JSON.stringify(newTasks));
+
+		//remove from UI
+		document.getElementById('task').removeChild(e.target);
+	}
+
+	e.preventDefault();
+}
